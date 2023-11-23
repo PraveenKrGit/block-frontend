ca4814649579551f1d11944cbb41b7f963b141e3
Praveen Kumar Yadav <30688435+PraveenKrGit@users.noreply.github.com>
2023-11-23 18:23:15 +0530

    asdfsfsd

--- src/App.js
+++ src/App.js
@@ -2,20 +2,31 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [ipAddress, setIpAddress] = useState(null);
+
+  const [ipAddress, setIPAddress] = useState('')
 
   useEffect(() => {
-    // Fetch the user's IP address when the component mounts
-    // axios.post('http://localhost:4000/record-ip')
-    axios.post('https://fusion-chain.onrender.com/record-ip')
-      .then(response => {
-        setIpAddress(response.data.ipAddress);
-        console.log(ipAddress);
-      })
-      .catch(error => {
-        console.error('Error fetsadfsching IP address:', error);
-      });
-  }, [ipAddress]);
+    fetch('https://api.ipify.org?format=json')
+      .then(response => response.json())
+      .then(data => setIPAddress(data.ip))
+      .catch(error => console.log(error))
+  }, []);
+
+
+  // const [ipAddress, setIpAddress] = useState(null);
+
+  // useEffect(() => {
+  //   // Fetch the user's IP address when the component mounts
+  //   // axios.post('http://localhost:4000/record-ip')
+  //   axios.post('https://fusion-chain.onrender.com/record-ip')
+  //     .then(response => {
+  //       setIpAddress(response.data.ipAddress);
+  //       console.log(ipAddress);
+  //     })
+  //     .catch(error => {
+  //       console.error('Error fetsadfsching IP address:', error);
+  //     });
+  // }, [ipAddress]);
 
   return (
     <div>
